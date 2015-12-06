12a0722c27c0a0eeab32ca2c791d4a30df5c7827
medyakovvit <medyakovvit@gmail.com>
2015-12-06 17:52:07 +0400

    Register class EditableImage in qml

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "editableimage.h"
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    qmlRegisterType<EditableImage>("My.EditableImage", 1, 0, "EditableImage");
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
